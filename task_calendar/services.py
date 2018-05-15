78f68cbcff6918935d53558421b3137087b6456c
flytrap <hiddenstat@gmail.com>
2018-05-15 15:46:05 +0800

    new: add calender subscription

--- task_calendar/services.py
+++ task_calendar/services.py
@@ -3,9 +3,9 @@
 #
 # Created by flytrap
 import datetime
-# from ics import Event, Calendar
-# from arrow import Arrow
-# from dateutil import tz as dateutil_tz
+from ics import Event, Calendar
+from arrow import Arrow
+from dateutil import tz as dateutil_tz
 from django.db.models import Q
 from taiga.projects.userstories.models import UserStory
 from taiga.projects.tasks.models import Task
@@ -25,105 +25,145 @@ class CalendarService(object):
         weekend = today + weekend_delta
         return weekend
 
+    @staticmethod
+    def get_ago_day(days=40):
+        today = datetime.date.today()
+        day_delta = datetime.timedelta(days)
+        day = today - day_delta
+        return day
+
     @classmethod
     def format_datetime(cls, item):
         """format datetime"""
-        # return Arrow(item.year, item.month, item.day, item.hour, item.minute)
+        return Arrow(item.year, item.month, item.day, item.hour, item.minute)
 
-    # @classmethod
-    # def get_event(cls, item):
-    #     e = Event()
-    #     e.name = item.subject
-    #     e.begin = cls.format_datetime(item.estimated_start)
-    #     e.end = cls.format_datetime(item.estimated_start)
-    #     e.description = item.description
-    #     return e
+    @classmethod
+    def get_event(cls, item):
+        e = Event()
+        e.name = item.subject
+        e.begin = cls.format_datetime(item.estimated_start)
+        e.end = cls.format_datetime(item.estimated_start)
+        e.description = item.description
+        return e
 
     @classmethod
     def get_ics(cls, user, start=None, end=None):
-        pass
-        # if start is None:
-        #     start = cls.get_monday()
-        # if end is None:
-        #     end = cls.get_weekend()
-        # userstories = cls.get_userstories(user, start, end)
-        # tasks = cls.get_tasks(user, start, end)
+        if start is None:
+            start = cls.get_ago_day()
+        userstories = cls.get_userstories(user, *cls.check_time(start, end))
+        tasks = cls.get_tasks(user, start, end)
 
-        # c = Calendar(creator=user.username)
-        # for usersotory in userstories:
-        #     e = cls.get_event(usersotory)
-        #     c.events.append(e)
-        # for task in tasks:
-        #     e = cls.get_event(task)
-        #     c.events.append(e)
-        # return c
+        c = Calendar(creator=user.username)
+        weeklies = {}
+        for task in tasks:
+            if task.user_story not in weeklies:
+                weeklies[task.user_story] = []
+                e = cls.get_event(task)
+                c.events.append(e)
+            weeklies[task.user_story].append(task)
+        for userstory in userstories:
+            if userstory not in weeklies:
+                weeklies[userstory] = []
+                e = cls.get_event(userstory)
+                c.events.append(e)
+        return c
 
     @classmethod
     def get_userstories(cls, user, start, end):
-        userstories = UserStory.objects.filter(assigned_to=user).filter(
-            (Q(estimated_start__lte=end) & Q(estimated_start__gte=start)) | (
-                Q(estimated_end__lte=end) & Q(estimated_end__gte=start)))
+        userstories = UserStory.objects.filter(assigned_to=user)
+        if start and end:
+            userstories = userstories.filter(
+                (Q(estimated_start__lte=end) & Q(estimated_start__gte=start)) | (
+                    Q(estimated_end__lte=end) & Q(estimated_end__gte=start)))
+        elif start:
+            userstories = userstories.filter(Q(estimated_start__gte=start) | Q(estimated_end__gte=start))
+        elif end:
+            userstories = userstories.filter(Q(estimated_start__lte=end) | Q(estimated_end__lte=end))
         return userstories
 
     @classmethod
     def get_tasks(cls, user, start, end):
-        tasks = Task.objects.filter(assigned_to=user).filter(
-            (Q(estimated_start__lte=end) & Q(estimated_start__gte=start)) | (
-                Q(estimated_end__lte=end) & Q(estimated_end__gte=start)))
+        tasks = Task.objects.filter(assigned_to=user)
+        if start and end:
+            tasks = tasks.filter(
+                (Q(estimated_start__lte=end) & Q(estimated_start__gte=start)) | (
+                    Q(estimated_end__lte=end) & Q(estimated_end__gte=start)))
+        elif start:
+            tasks = tasks.filter(Q(estimated_start__gte=start) | Q(estimated_end__gte=start))
+        elif end:
+            tasks = tasks.filter(Q(estimated_start__lte=end) | Q(estimated_end__lte=end))
         return tasks
+
+    @staticmethod
+    def check_time(start, end):
+        if start:
+            try:
+                start = datetime.datetime.strptime(start, "%Y-%m-%d").date()
+            except:
+                pass
+        if end:
+            try:
+                end = datetime.datetime.strptime(end, "%Y-%m-%d").date()
+            except:
+                pass
+        return start, end
 
 
 class WeeklyObj(object):
     def __init__(self, user, start=None, end=None):
         self.user = user
         self.delta = datetime.timedelta(7)
-        self.start = CalendarService.get_monday()
-        self.end = CalendarService.get_weekend()
-        self.check_time(start, end)
-        self.weeklies = {}
-
-    def check_time(self, start, end):
-        if start:
-            try:
-                self.start = datetime.datetime.strptime(start, "%Y-%m-%d").date()
-            except:
-                pass
-        if end:
-            try:
-                self.end = datetime.datetime.strptime(end, "%Y-%m-%d").date()
-            except:
-                pass
+        if not start:
+            start = CalendarService.get_monday()
+        if not end:
+            end = CalendarService.get_weekend()
+        self.start, self.end = CalendarService.check_time(start, end)
 
     def get_title(self):
-        return '# weekly ({}->{})\n'.format(self.start, self.end)
+        return '# report ({} to {})\n'.format(self.start, self.end)
 
     def get_weekly(self):
+        weeklies = {}
         tasks = CalendarService.get_tasks(self.user, self.start, self.end).order_by('estimated_start')
         userstories = CalendarService.get_userstories(self.user, self.start, self.end).order_by('estimated_start')
         for task in tasks:
-            if task.user_story not in self.weeklies:
-                self.weeklies[task.user_story] = []
-            self.weeklies[task.user_story].append(task)
+            if task.user_story not in weeklies:
+                weeklies[task.user_story] = []
+            weeklies[task.user_story].append(task)
         for userstory in userstories:
-            if userstory not in self.weeklies:
-                self.weeklies[userstory] = []
+            if userstory not in weeklies:
+                weeklies[userstory] = []
+        return weeklies
 
     def get_result(self):
-        content = self.get_title()
-        self.get_weekly()
-        for us, tasks in self.weeklies.items():
+        content = ''
+        weeklies = self.get_weekly()
+        for us, tasks in weeklies.items():
             for task in tasks:
                 content += self.get_content(task, parent=us)
             else:
                 content += self.get_content(us)
         return content
 
+    def get_report(self):
+        content = self.get_title()
+        content += '## current week task: \n'
+        content += self.get_result()
+        detail = self.end - self.start + datetime.timedelta(1)
+        self.start += detail
+        self.end += detail
+        plan = self.get_result()
+        if plan:
+            content += '## next week plan: \n'
+            content += plan
+        return content
+
     @staticmethod
     def get_content(item, prefix='* ', parent=None):
         content = prefix
         if item.estimated_start and item.estimated_end:
-            content += '({}->{})'.format(item.estimated_start.strftime('%m-%d %H:%M'),
-                                         item.estimated_end.strftime('%m-%d %H:%M'))
+            content += '({}->{}) '.format(item.estimated_start.strftime('%m-%d %H:%M'),
+                                          item.estimated_end.strftime('%m-%d %H:%M'))
         if parent:
             content += '{}:'.format(parent.subject)
         content += item.subject
--- task_calendar/views.py
+++ task_calendar/views.py
@@ -1,26 +1,45 @@
-from django.shortcuts import render
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Created by flytrap
+from io import StringIO
+from django.contrib.auth import get_user_model
+from django.http.response import HttpResponse, StreamingHttpResponse
 
-# Create your views here.
 from taiga.base.api.viewsets import GenericViewSet
-from taiga.base.decorators import list_route, detail_route
+from taiga.base.decorators import list_route
 from taiga.base import response
 from .services import CalendarService, WeeklyObj
+
+User = get_user_model()
 
 
 class CalenderViewSet(GenericViewSet):
     @list_route(methods=["GET"])
     def ics(self, request):
+        user = request.user
         if not request.user.is_authenticated:
-            return response.Unauthorized()
-        CalendarService.get_ics(request.user)
-        return response.Ok()
+            user_id = self.request.QUERY_PARAMS.get('user_id')
+            user = User.objects.filter(id=user_id).first()
+            if not user:
+                return response.Unauthorized()
+        c = CalendarService.get_ics(user, *self.parser_params())
+        f = StringIO()
+        for line in c:
+            f.writelines(line)
+        f.seek(0, 0)
+        resp = HttpResponse(f, content_type='text/calendar; charset=UTF-8')
+        return resp
+
+    def perform_content_negotiation(self, request, force=True):
+        return super(CalenderViewSet, self).perform_content_negotiation(request, force)
 
     @list_route(methods=["GET"])
     def weekly(self, request):
         if not request.user.is_authenticated:
             return response.Unauthorized()
         start, end = self.parser_params()
-        data = WeeklyObj(request.user, start, end).get_result()
+        data = WeeklyObj(request.user, start, end).get_report()
         return response.Ok(data)
 
     def parser_params(self):
